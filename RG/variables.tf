f26db3c84f2c1cf893fe105f702f548b9045d3e9
Agam <aroraagam03@gmail.com>
2024-08-27 12:59:09 +0530

    contains variables used in main.tf

--- RG/variables.tf
+++ RG/variables.tf
@@ -1,1 +1,11 @@
+  variable "rglocation" {
+    type        = string
+    description = "The location where the resource group will be created"
+    default     = "West US"
+  }
+  variable "rg_set" {
+    description = "List of resource groups"
+    type        = set(string)
+    default     = ["agamrg1", "agamrg2"]
+  }
 
