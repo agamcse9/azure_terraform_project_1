1d44df122fe580b8ef4ffecd6b89fbf1e5083372
Agam <aroraagam03@gmail.com>
2024-09-08 18:13:58 +0530

    Delete Resource group & Storage account Using Maps directory

--- Resource group & Storage account Using Maps/resource_group.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "rgs" {
-  for_each = var.rg_map
-  name     = each.key
-  location = each.value
-}
--- Resource group & Storage account Using Maps/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "rg_map" {
-  type = map(any)
-  default = {
-    rg1 = "westeurope"
-    rg2 = "centralindia"
-    rg3 = "canadacentral"
-  }
-}
